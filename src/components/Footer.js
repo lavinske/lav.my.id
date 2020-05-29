abc4c1188aaa58049d2339386a480f249224b1a3
Steven Lavinske <stevenlavinske@gmail.com>
2020-05-29 20:45:52 +0700

    finalizing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,20 +8,20 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className="footer container">
+        // <div>
+        //   <a href="https://ko-fi.com/taniarascia" target="_blank" rel="noopener noreferrer">
+        //     Ko-Fi
+        //   </a>
+        //   <a href="https://patreon.com/taniarascia" target="_blank" rel="noopener noreferrer">
+        //     Patreon
+        //   </a>
+        //   <Link to="/contact/">Contact</Link>
+        //   <a href="https://www.taniarascia.com/rss.xml" target="_blank" rel="noopener noreferrer">
+        //     RSS
+        //   </a>
+        // </div>
         <div>
-          <a href="https://ko-fi.com/taniarascia" target="_blank" rel="noopener noreferrer">
-            Ko-Fi
-          </a>
-          <a href="https://patreon.com/taniarascia" target="_blank" rel="noopener noreferrer">
-            Patreon
-          </a>
-          <Link to="/contact/">Contact</Link>
-          <a href="https://www.taniarascia.com/rss.xml" target="_blank" rel="noopener noreferrer">
-            RSS
-          </a>
-        </div>
-        <div>
-          <a href="https://github.com/taniarascia" title="Open-source on GitHub" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/lavinske" title="Open-source on GitHub" target="_blank" rel="noopener noreferrer">
             <img
               src={github}
               className="footer-img"
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import tania from '../../content/images/maxine.jpg'
+import maxine from '../../content/images/maxine.jpg'
 import patreon from '../../content/thumbnails/patreon.png'
 import kofi from '../../content/thumbnails/kofi.png'
 
@@ -10,35 +10,33 @@ export default class UserInfo extends Component {
         <div className="container note-container">
           <div className="flex-author">
             <div className="flex-avatar">
-              <img className="avatar" src={tania} alt="Tania Rascia" />
+              <img className="avatar" src={maxine} alt="Steven Lavinske" />
             </div>
             <div>
               <h3>Author</h3>
               <p>
-                Hey, I’m Tania, a full stack software engineer. I write about what I know to help
-                viewers like you. My site has <strong>no ads, sponsors, or bullshit.</strong> If you
-                enjoy my content, please consider supporting what I do!
+                Hey, I’m Steven Lavinske, an university student. I'm now learning Gatsby.js
               </p>
 
-              <div className="flex">
-                <a
-                  href="https://ko-fi.com/taniarascia"
-                  className="donate-button"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={kofi} className="coffee-icon" alt="Coffee icon" />
-                  Buy me a coffee
-                </a>
-                <a
-                  className="patreon-button"
-                  href="https://www.patreon.com/taniarascia"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={patreon} height="50" width="50" alt="Patreon" /> Become a Patron
-                </a>
-              </div>
+              // <div className="flex">
+              //   <a
+              //     href="https://ko-fi.com/taniarascia"
+              //     className="donate-button"
+              //     target="_blank"
+              //     rel="noopener noreferrer"
+              //   >
+              //     <img src={kofi} className="coffee-icon" alt="Coffee icon" />
+              //     Buy me a coffee
+              //   </a>
+              //   <a
+              //     className="patreon-button"
+              //     href="https://www.patreon.com/taniarascia"
+              //     target="_blank"
+              //     rel="noopener noreferrer"
+              //   >
+              //     <img src={patreon} height="50" width="50" alt="Patreon" /> Become a Patron
+              //   </a>
+              // </div>
             </div>
           </div>
         </div>
