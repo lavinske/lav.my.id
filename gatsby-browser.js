d2189a0a06a8fef97e9e770b47940c660625bd47
Steven Lavinske <stevenlavinske@gmail.com>
2020-05-30 12:39:06 +0700

    Core Engine Edit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import { ThemeProvider } from './src/context/ThemeContext'
+import './src/styles/main.scss'
 
 export const wrapRootElement = ({ element }) => <ThemeProvider>{element}</ThemeProvider>
